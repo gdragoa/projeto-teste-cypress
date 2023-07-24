02788b69a45266113cd4c8a6c29e4d632261ab2a
Ana Carolina <anagamaral@outlook.com>
2023-07-24 00:22:02 -0300

    Adição de custom commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,15 @@
 
 Cypress.Commands.add('addToCart', () => {
     cy.get('button path[d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"]').click();
+    cy.get('.MuiInputBase-input').should('have.value', '1');
     cy.contains('button', 'continuar').click();
+
+})
+
+
+Cypress.Commands.add('emptyCart', () => {
+    cy.get('.MuiInputBase-input').should('have.value', '0');
+    cy.contains('button', 'continuar').should('be.disabled');
 
 })
 
@@ -25,7 +33,34 @@ Cypress.Commands.add('register', (
 
 })
 
-Cypress.Commands.add('registerVoucher', () => {
-    cy.contains('button', 'Salvar comprovante').click()
+Cypress.Commands.add('registerEmptyFields', () => {
+    cy.contains('div', 'Nome completo').within(() => {
+        cy.get('input').should('be.empty');
+    });
+
+    cy.get('.MuiInputBase-root select').should('have.value', '');
+
+    cy.contains('div', 'Número').within(() => {
+        cy.get('input').should('be.empty');
+    });
+
+    cy.contains('div', 'E-mail').within(() => {
+        cy.get('input').should('be.empty');
+    });
+
+    cy.contains('button', 'finalizar').should('be.disabled')
 
 })
+
+Cypress.Commands.add('registerVoucher', () => {
+    cy.contains('p', 'Código da inscrição')
+        .next()
+        .invoke('text')
+        .then((textoDoElemento) => {
+            cy.contains('button', 'Salvar comprovante').click();
+            cy.wait(400)
+            cy.readFile(`cypress/downloads/comprovante-inscricao_${textoDoElemento}.pdf`).should('exist');
+        });
+});
+
+
